9cf006e296e5ce96029517fdaf4b8d78240d99cb
raymondaing <raymondaing@gmail.com>
2021-02-21 21:47:00 -0500

    Guardian updates
    - Removed create method from manager in favor of default manager create function
    - replaced filter with get (assures we return the object of a Guardian using a PK)
    - Model methods for instance get/sets

--- src/db/managers/guardian_manager.py
+++ src/db/managers/guardian_manager.py
@@ -5,23 +5,19 @@ from ..models import guardian
 
 
 class GuardianManager(models.Manager):
-    # Manager for table-level methods on the Guardian model
-    # noinspection PyMethodMayBeStatic
-    def create_new_guardian(self, first_name_: str, last_name_: str, phone_number_: str, time_start_: datetime.time,
-                            time_end_: datetime.time, pref_lang_: str):
-        # Create a new guardian and add to FNF database
-        # todo: add arg validation
-        new = guardian.Guardian(first_name=first_name_, last_name=last_name_, phone_number=phone_number_,
-                                time_start=time_start_, time_end=time_end_, preferred_language=pref_lang_)
-        new.save()
-
-    def search_for_guardian_by_number(self, phone_number_: str):
+    def get_guardian_by_number(self, phone_number_: str):
         # Query the database using number
-        return self.filter(phone_number=phone_number_)
+        # Number is primary key, always one instance
+        return self.get(phone_number=phone_number_)
 
-    def search_for_guardian_by_name(self, first_name_: str, last_name_: str = None):
-        # Query the database using number
+    def get_guardian_by_id(self, guardian_id_: str):
+        # Query the database using id
+        # Id is primary key, always one instance
+        return self.get(guardian_id=guardian_id_)
+
+    def search_for_guardians_by_name(self, first_name_: str, last_name_: str = None):
+        # Query the database using name
         queryset = self.filter(first_name__iexact=first_name_)
         if last_name_ is not None:
             queryset = queryset.filter(last_name__iexact=last_name_)
-        return queryset
+        return list(queryset)
--- src/db/models/guardian.py
+++ src/db/models/guardian.py
@@ -3,6 +3,7 @@ from django.core.validators import RegexValidator
 from .. import languages_and_countries as lang
 import uuid
 from ..managers import guardian_manager
+import datetime
 
 
 class Guardian(models.Model):
@@ -14,7 +15,57 @@ class Guardian(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number format must be: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, unique=True)  # validators should be a list
-    time_start = models.TimeField()
-    time_end = models.TimeField()
+    time_start = models.TimeField(default=datetime.time(hour=8))
+    time_end = models.TimeField(default=datetime.time(hour=22))
     preferred_language = models.CharField(max_length=2, choices=lang.languages)
     objects = guardian_manager.GuardianManager()
+
+    def get_id(self):
+        return self.guardian_id
+
+    def get_first_name(self):
+        return self.first_name
+
+    def get_last_name(self):
+        return self.last_name
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_time_start(self):
+        return self.time_start
+
+    def get_time_end(self):
+        return self.time_end
+
+    def get_preferred_language(self):
+        # returns a tuple of the ISO format and human-readable format (ex. ( 'en', 'English')
+        pair = (self.preferred_language, self.get_preferred_language_display())
+        return pair
+
+    def set_first_name(self, new_first_name: str):
+        self.first_name = new_first_name
+        self.save(update_fields=['first_name'])
+
+    def set_last_name(self, new_last_name: str):
+        self.last_name = new_last_name
+        self.save(update_fields=['last_name'])
+
+    def set_phone_number(self, new_phone_number: str):
+        # phone number format is '+999999999'
+        self.phone_number = new_phone_number
+        self.save(update_fields=['phone_number'])
+
+    def set_time_start(self, new_time_start):
+        self.time_start = new_time_start
+        self.save(update_fields=['time_start'])
+
+    def set_time_end(self, new_time_end):
+        self.time_end = new_time_end
+        self.save(update_fields=['time_end'])
+
+    def set_preferred_language(self, new_lang_iso):
+        # use iso language codes
+        if new_lang_iso in lang.languages:
+            self.preferred_language = new_lang_iso
+        self.save(update_fields=['preferred_language'])
