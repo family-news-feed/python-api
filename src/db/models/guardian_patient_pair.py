d270552b23ed1c6fa00e27d3ef6edf9afb4bf16d
JoshuaKir <34132786+JoshuaKir@users.noreply.github.com>
2021-02-24 01:29:27 -0500

    guardian patient pair manager + model methods

--- src/db/models/guardian_patient_pair.py
+++ src/db/models/guardian_patient_pair.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from .. import languages_and_countries as lang
+from ..managers import guardian_patient_pair_manager
 
 
 class GuardianPatientPair(models.Model):
@@ -8,3 +8,16 @@ class GuardianPatientPair(models.Model):
     guardian = models.ForeignKey('Guardian', on_delete=models.CASCADE)
     patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+    objects = guardian_patient_pair_manager.GuardianPatientPairManager()
+
+    def get_id(self):
+        return self.pair_id
+
+    def get_guardian_id(self):
+        return self.guardian
+
+    def get_patient_mrn(self):
+        return self.patient
+
+    def get_date_created(self):
+        return self.date_created
