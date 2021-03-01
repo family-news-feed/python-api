f510add292652b6c9be00122552c358512c6e5d1
JoeB98 <ellisrambles@gmail.com>
2021-03-01 18:54:18 -0500

    Added patient event manager/model

--- src/db/models/approved_patient_event.py
+++ src/db/models/approved_patient_event.py
@@ -1,8 +1,9 @@
 from django.db import models
 from ..event_types import event_types
-
+from ..managers import approved_patient_event_manager
 
 class ApprovedPatientEvent(models.Model):
     id = models.BigAutoField(primary_key=True)
     patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
     event_type = models.CharField(max_length=15, choices=event_types)
+    objects = approved_patient_event_manager.ApprovedPatientEventManager()
