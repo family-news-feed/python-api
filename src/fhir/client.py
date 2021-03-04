7068fd05fe3f984ef4b06b5e25fc582f7e8db169
Ben Burke <benjamindburke@gmail.com>
2021-03-04 18:23:04 -0500

    fhir auth with session caching

--- src/fhir/client.py
+++ src/fhir/client.py
@@ -1,44 +1,38 @@
-import os
-from fhirclient.client import FHIRClient as client
+import functools
+from django.conf import settings
+from fhirclient.client import FHIRClient
 
-# FHIR Data Access Scopes
-scopes = [
-    'offline_access',
-    'system/Appointment.read',
-    'system/CarePlan.read',
-    'system/Encounter.read',
-    'system/MedicationAdministration.read',
-    'system/MedicationOrder.read',
-    'system/Observation.read',
-    'system/Patient.read',
-    'system/Procedure.read',
-]
-
-# FHIR Auth Handshake Settings
-SETTINGS = {
-    'app_id': os.environ.get('DJANGO_FHIR_APP_ID'),
-    'api_base': os.environ.get('DJANGO_FHIR_API_BASE'),
-    'app_secret': os.environ.get('DJANGO_FHIR_CLIENT_ID'),
-    # the parameter is called redirect_uri but it is URL scheme not uri (encoded URL)
-    'redirect_uri': os.environ.get('DJANGO_FHIR_REDIRECT_URL'),
-    'scope': ' '.join(scopes),
-}
-
-class FHIRClient(client):
+class FHIR(FHIRClient):
     ''' Cached connection manager for the FHIR database.
     '''
 
-    def __init__(self, **kwargs):
-        save_func = kwargs.get('save_func')
-        state = kwargs.get('state')
-        if state:
+    def __init__(self, session, **kwargs):
+        save_func = functools.partial(FHIR._save_fhir_state, session, settings.DJANGO_SESSION_KEY)
+        state = session.get(settings.DJANGO_SESSION_KEY)
+        if state is not None:
             super().__init__(state=state, save_func=save_func)
         else:
-            settings = SETTINGS.copy()
-            launch_token = kwargs.get('launch_token')
-            if launch_token:
-                settings.launch_token = launch_token
-            patient_id = kwargs.get('patient_id')
-            if patient_id:
-                settings.patient_id = patient_id
-            super().__init__(settings=settings, save_func=save_func)
+            sett = settings.FHIR_SETTINGS.copy()
+            for arg in kwargs:
+                sett[arg] = kwargs[arg]
+            super().__init__(settings=sett, save_func=save_func)
+        self.save_state()
+
+    @staticmethod
+    def _save_fhir_state(session, key, state):
+        session[key] = state
+
+    @staticmethod
+    def get_session(request, **kwargs):
+        return FHIR(request.session, **kwargs)
+
+    @staticmethod
+    def logout_session(request):
+        if request.session is not None and settings.DJANGO_SESSION_KEY in request.session:
+            smart = FHIR.get_session(request)
+            smart.reset_patient()
+
+    @staticmethod
+    def reset_session(request):
+        if request.session is not None and settings.DJANGO_SESSION_KEY in request.session:
+            del request.session[settings.DJANGO_SESSION_KEY]
