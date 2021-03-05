d12186348035a380fb8de9d6879483f4d955e6a3
raymondaing <raymondaing@gmail.com>
2021-03-05 02:00:56 -0500

    fixed managers

--- src/db/managers/guardian_notification_instance_manager.py
+++ src/db/managers/guardian_notification_instance_manager.py
@@ -13,7 +13,7 @@ class GuardianNotificationInstanceManager(models.Manager):
 
     def search_logs_by_pair_id(self, pair_id_: str):
         # Query instances and filter by pair id
-        matching_instances = self.filter(guardian_patient_pair_id=pair_id_)
+        matching_instances = self.filter(guardian_patient_pair=pair_id_)
 
         return list(matching_instances)
 
--- src/db/managers/guardian_patient_pair_manager.py
+++ src/db/managers/guardian_patient_pair_manager.py
@@ -7,7 +7,7 @@ class GuardianPatientPairManager(models.Manager):
 
     def get_patient_mrns_from_guardian(self, guardian_id_: str):
         # Query the database for the patients related to a guardian
-        pairs_with_guardian = self.filter(guardian_id=guardian_id_)
+        pairs_with_guardian = self.filter(guardian=guardian_id_)
 
         patient_mrns = []
         for pair in pairs_with_guardian:
@@ -16,7 +16,7 @@ class GuardianPatientPairManager(models.Manager):
 
     def get_guardian_ids_from_patients(self, patient_mrn_: str):
         # Query the database for the guardians related to a patient
-        pairs_with_patient = self.filter(patient_mrn=patient_mrn_)
+        pairs_with_patient = self.filter(patient=patient_mrn_)
 
         guardian_ids = []
         for pair in pairs_with_patient:
@@ -25,7 +25,7 @@ class GuardianPatientPairManager(models.Manager):
 
     def get_pair_id(self, guardian_id_: str, patient_mrn_: str):
         # Query the database using guardian id and patient mrn
-        pair = self.get(guardian_id=guardian_id_, patient_id=patient_mrn_)
+        pair = self.get(guardian=guardian_id_, patient=patient_mrn_)
         return pair.pair_id
 
     def get_pair_creation_time(self, guardian_id_: str, patient_mrn_: str):
