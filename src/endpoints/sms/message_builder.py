87fca1823f5882a1fc64678aea946841c7047abf
raymondaing <raymondaing@gmail.com>
2021-03-08 21:08:23 -0500

    Update message_builder.py
    fixed message templates

--- src/endpoints/sms/message_builder.py
+++ src/endpoints/sms/message_builder.py
@@ -2,15 +2,16 @@
 
 class MessageBuilder:
     welcome = "From Children's National:\n"
-    optout = "Reply 'STOP' to opt-out of texts.\n"
+    optout = "Reply 'STOP' to opt-out of texts."
 
     def careteam_message(self, full_name, role):
-        msg = self.welcome + "{}{} has been added to your active CareTeam\n".format(full_name, role) + self.optout
+        msg = self.welcome + "{}{} has been added to your active CareTeam.\n".format(full_name, role) + self.optout
         return msg
 
     def encounter_move_message(self, location):
-        msg = self.welcome + "Patient has been moved to {}\n".format(location) + self.optout
+        msg = self.welcome + "Patient has been moved to {}.\n".format(location) + self.optout
         return msg
 
     def nutrition_message(self, item, time):
-        msg = self.welcome + "Patient received {} at {}\n".format(item, time) +  self.optout
+        msg = self.welcome + "Patient received {} at {}.\n".format(item, time) +  self.optout
+        return msg
